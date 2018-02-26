edd269518536e2b2218d25dbd5707d6ace6797d0
OBellon <OscarBellon96@gmail.com>
2018-02-26 16:49:42 +0100

    Delete config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyAi___01lYsrHH4j_RYqpbG1fvo4Hz17LY",
-  authDomain: "mad-ping-pong.firebaseapp.com",
-  databaseURL: "https://mad-ping-pong.firebaseio.com",
-  projectId: "mad-ping-pong",
-  storageBucket: "mad-ping-pong.appspot.com",
-  messagingSenderId: "450179736070"
-};
